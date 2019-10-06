8faf0d99922b941fb4f5bb6907feb363f9e38686
AmRo <virtualemailbox045@gmail.com>
2019-10-06 19:29:01 +0330

    Create DNS services

---
+++ Source/Shecan/Core/Network/IDnsService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Shecan.Models;
+
+namespace Shecan.Core.Network
+{
+    public interface IDnsService
+    {
+        event EventHandler DnsChanged;
+
+        void Set(Dns dns);
+        void Unset();
+        Dns GetCurrentDns();
+        string GetCurrentInterfaceAlias();
+    }
+}

d60c97a512563d52321a7fd8284a96d52fb5c83b
AmRo <virtualemailbox045@gmail.com>
2019-10-06 16:06:47 +0330

    Create Box user control

---
+++ Source/Shecan/Contracts/IBox.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Shecan.Contracts
+{
+    public interface IBox
+    {
+        Action CloseCallback { get; set; }
+        Action CompleteCallback { get; set; }
+    }
+}

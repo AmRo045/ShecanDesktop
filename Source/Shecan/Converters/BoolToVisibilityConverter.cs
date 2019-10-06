e1d0113c13cc556175886fee8f53cf13859e9a2a
AmRo <virtualemailbox045@gmail.com>
2019-10-06 16:06:19 +0330

    Create BoolToVisibilityConverter.cs

---
+++ Source/Shecan/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Shecan.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isReverse = false;
+
+            if (parameter != null)
+                isReverse = System.Convert.ToBoolean(parameter);
+
+            var status = System.Convert.ToBoolean(value);
+
+            if (isReverse)
+                return status ? Visibility.Visible : Visibility.Collapsed;
+
+            return !status ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

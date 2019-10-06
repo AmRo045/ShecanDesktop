4d03242d306e483b7fabe024842b4252e68cf078
AmRo <virtualemailbox045@gmail.com>
2019-10-06 16:08:38 +0330

    Add BoxHost and main frame to MainWIndow

--- Source/Shecan/MainWindow.xaml.cs
+++ Source/Shecan/MainWindow.xaml.cs
@@ -1,10 +1,49 @@
-﻿namespace Shecan
+﻿using System;
+using System.Windows;
+using Shecan.Contracts;
+using Shecan.Views;
+
+namespace Shecan
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            BoxHost.ParentElement = MainFrame;
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            MainFrame.NavigationService.Navigate(new MainPage());
+        }
+
+        public void DisplayBox(IBox box, bool closeOnClickAway = true)
+        {
+            box.CloseCallback = BoxCloseCallback;
+
+            BoxHost.CloseOnClickAway = closeOnClickAway;
+            BoxHost.Show(box);
+        }
+
+        public void DisplayBox(IBox box, Action completeCallback, bool closeOnClickAway = true)
+        {
+            box.CloseCallback = BoxCloseCallback;
+            box.CompleteCallback = completeCallback;
+
+            BoxHost.CloseOnClickAway = closeOnClickAway;
+            BoxHost.Show(box);
+
+        }
+
+        private void BoxCloseCallback()
+        {
+            BoxHost.Close();
+        }
+
+        public void CloseBox()
+        {
+            BoxCloseCallback();
         }
     }
 }
